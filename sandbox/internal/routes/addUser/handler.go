4721f688db565566f48665610f5bd647e094008f
matesmoutinho <matesmoutinho01@gmail.com>
2026-09-23 03:41:46 -0300

    refactor: pass response as pointer in InterfaceHandler

--- sandbox/internal/routes/addUser/handler.go
+++ sandbox/internal/routes/addUser/handler.go
@@ -4,7 +4,7 @@ import (
 	"github.com/MateusMoutinhoOrg/AgnosTeset/sandbox/api"
 )
 
-func InterfaceHandler(sandbox *api.Sandbox, entries *Entries, response api.Response) error {
+func InterfaceHandler(sandbox *api.Sandbox, entries *Entries, response *api.Response) error {
 	response.SetStatus(200)
 	response.WriteBody([]byte("Hello World"))
 	return nil
