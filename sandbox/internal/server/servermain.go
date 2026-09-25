929aa3ac006b32f556900b0e35f46f83c8daa7d7
matesmoutinho <matesmoutinho01@gmail.com>
2026-09-25 06:59:37 -0300

    docs: add implementation comment for server startup in ServerMain

--- sandbox/internal/server/servermain.go
+++ sandbox/internal/server/servermain.go
@@ -5,6 +5,6 @@ import (
 )
 
 func ServerMain(sandbox *api.Sandbox, props api.ServeProps) error {
-
+	//needs to start the server and handle sandbox.routes based on if is Actionable and priority levels .
 	return nil
 }
