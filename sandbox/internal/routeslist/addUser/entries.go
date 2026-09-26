0445235be865b50c079d61f8ff935fcb064098f3
matesmoutinho <matesmoutinho01@gmail.com>
2026-09-26 04:40:50 -0300

    chore: remove all project files and documentation

--- sandbox/internal/routeslist/addUser/entries.go
+++
@@ -1,9 +0,0 @@
-package adduser
-
-type Entries struct {
-	AddUser   string `id:"AddUser"`
-	FullRoute string `id:"FullRoute"`
-	Name      string `id:"Name"`
-	Email     string `id:"Email"`
-	Password  string `id:"Password"`
-}
