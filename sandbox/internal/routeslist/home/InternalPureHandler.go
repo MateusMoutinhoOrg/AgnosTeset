0d6b441f149d6ee5676bb8b629ce622e98b08064
matesmoutinho <matesmoutinho01@gmail.com>
2026-09-26 06:41:47 -0300

    feat: serve embedded frontend static files in route handler

--- sandbox/internal/routeslist/home/InternalPureHandler.go
+++ sandbox/internal/routeslist/home/InternalPureHandler.go
@@ -17,8 +17,67 @@ import (
 // routeio.Fail; nil means "done" or "not mine".
 func InternalPureHandler(sandbox *api.Sandbox, route *api.Route, entries *Entries, response *serverdeps.Response) error {
 
+	dest := entries.Rest
+	if dest == "" {
+		dest = "index.html"
+	}
+	if sandbox.Deps.Stringsdeps.HasPrefix(dest, "/") {
+		dest = sandbox.Deps.Stringsdeps.TrimPrefix(dest, "/")
+	}
+
+	fullPath := "frontend/" + dest
+	content, err := sandbox.Deps.Embeddeps.ReadFile(fullPath)
+
+	if err != nil && !sandbox.Deps.Stringsdeps.HasSuffix(dest, ".html") {
+		// Test if dest + "index.html" exists as requested
+		altDest := dest
+		if altDest != "" && !sandbox.Deps.Stringsdeps.HasSuffix(altDest, "/") {
+			altDest += "/"
+		}
+		altDest += "index.html"
+		altPath := "frontend/" + altDest
+
+		if content2, err2 := sandbox.Deps.Embeddeps.ReadFile(altPath); err2 == nil {
+			content = content2
+			err = nil
+			fullPath = altPath
+		}
+	}
+
+	if err != nil {
+		response.SetStatus(api.StatusNotFound)
+		response.Write([]byte("Not Found"))
+		return nil
+	}
+
+	ext := ""
+	lastDot := sandbox.Deps.Stringsdeps.LastIndex(fullPath, ".")
+	lastSlash := sandbox.Deps.Stringsdeps.LastIndex(fullPath, "/")
+	if lastDot != -1 && lastDot > lastSlash {
+		ext = fullPath[lastDot:]
+	}
+
+	contentType := "text/plain"
+	switch ext {
+	case ".html":
+		contentType = "text/html"
+	case ".css":
+		contentType = "text/css"
+	case ".js":
+		contentType = "application/javascript"
+	case ".png":
+		contentType = "image/png"
+	case ".jpg", ".jpeg":
+		contentType = "image/jpeg"
+	case ".svg":
+		contentType = "image/svg+xml"
+	case ".json":
+		contentType = "application/json"
+	}
+
+	response.SetHeader("Content-Type", contentType)
 	response.SetStatus(api.StatusOk)
-	response.Write([]byte("home called\n"))
+	response.Write(content)
 
 	return nil
 }
